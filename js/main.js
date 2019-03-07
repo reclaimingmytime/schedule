eb97495d06fe04e9e99ddde8ff18a45df810be4b
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2019-03-07 14:31:29 +0100

    Removed arrow key nav entirely

--- js/main.js
+++ js/main.js
@@ -26,24 +26,20 @@ $(function () {
 	/* Keyboard navigation */
 	$(document).keydown(function (e) {
 		switch (e.which) {
-			// case 37: // left
 			case 65: // A
 				if ($('head').data('prevday') !== "none") {
 					window.location.href = "?d=" + $('head').data('prevday');
 				}
 				break;
 
-				// case 39: // right
 			case 68: // D
 				window.location.href = "?d=" + $('head').data('nextday');
 				break;
 
-				// case 38: // up
 			case 87: // W
 				window.location.href = "?d=" + $('head').data('nextweek');
 				break;
 
-				// case 40: // down
 			case 83: // S
 				if ($('head').data('prevweek') !== "none") {
 					window.location.href = "?d=" + $('head').data('prevweek');
