8ef7ec84bcdac20a70dcdd4f6a6115d4a7cb59a2
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2019-03-11 17:57:04 +0100

    Decreased scroll to top time

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ $(function () {
 	$('.top').on('click', function () {
 		$('html, body').animate({
 			scrollTop: 0
-		}, 400);
+		}, 300);
 		return false;
 	});
 	if (($(document).height() > $(window).height())) { //if content is scrollable
