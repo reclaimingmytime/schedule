de449f86629a71e52cc31b3bd4052591c72c3bfe
reclaimingmytime <creator@hotmail.de>
2019-05-16 12:29:12 +0200

    Try fix for Mobile Safari bug

--- include/data-presentation.php
+++ include/data-presentation.php
@@ -49,6 +49,9 @@ $highlightClasses = 'bg-dark text-light';
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
 		<style>
+		html {
+			height: 100%;
+		}
 		.dropdown-toggle {
 			outline: none;
 		}
