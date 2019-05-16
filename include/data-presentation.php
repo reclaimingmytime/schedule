7ecbb26bff01beab63927395e51a2df635969344
reclaimingmytime <creator@hotmail.de>
2019-05-16 12:32:24 +0200

    Use body instead of html

--- include/data-presentation.php
+++ include/data-presentation.php
@@ -49,7 +49,7 @@ $highlightClasses = 'bg-dark text-light';
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
 		<style>
-		html {
+		body {
 			height: 100%;
 		}
 		.dropdown-toggle {
