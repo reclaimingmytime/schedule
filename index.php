380ae72d057306675beb6bad10bd5e22020ce7bb
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2019-09-29 00:04:23 +0200

    remove config file notice

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!file_exists("config.php")) {
-	die("config.php missing. Please use config.default.php as a template (if available).");
+	die("config file missing.");
 }
 require_once("config.php");
 require_once("include/globals.php");
