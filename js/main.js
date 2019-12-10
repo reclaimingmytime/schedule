fa1a91ccfafc033af0c79d29b58f51a5fa2c9ee7
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2019-12-10 14:18:19 +0100

    only reload for when week overview is selected

--- js/main.js
+++ js/main.js
@@ -289,7 +289,7 @@ $(function () {
 		const time = formatTime(dt.getHours(), dt.getMinutes());
 		const timeMilliseconds = dt.getTime();
 		
-		if(displayedDt != dt.getDate()) {
+		if(displayedDt != dt.getDate() && head.data('weekoverview') === true) {
 			window.location.reload();
 		}
 		
