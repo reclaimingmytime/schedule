dd20d691164c3cbde6995464708a30e2db89504d
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2020-02-05 01:10:57 +0100

    also add auto-refresh to day overview

--- js/main.js
+++ js/main.js
@@ -292,7 +292,7 @@ $(function () {
 		const time = formatTime(dt.getHours(), dt.getMinutes());
 		const timeMilliseconds = dt.getTime();
 		
-		if(displayedDt != dt.getDate() && head.data('weekoverview') === true) {
+		if(displayedDt != dt.getDate()) {
 			window.location.reload();
 		}
 		
