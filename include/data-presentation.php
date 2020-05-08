d5e414c3bb2a982c5871e0c35312c6a3cfe13e6a
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2020-05-08 17:07:41 +0200

    request credentials for serviceworker

--- include/data-presentation.php
+++ include/data-presentation.php
@@ -109,7 +109,7 @@ $hasManifest = isset($manifest) && !empty($manifest);
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous">
 		<?php if($hasManifest == true) { ?>
-			<link rel="manifest" href="site.webmanifest.php">	
+			<link rel="manifest" href="site.webmanifest.php" crossorigin="use-credentials">	
 		<?php } ?>
 		<?php if(!empty($touchIconPath)) { ?>
 			<link rel="apple-touch-icon" href="<?php echo $touchIconPath; ?>">	
