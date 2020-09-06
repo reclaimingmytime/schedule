d5f8bf65c2ba9f0c751ef26e845aaed14d43601b
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2020-09-06 16:57:40 +0200

    add required days

--- include/data-presentation.php
+++ include/data-presentation.php
@@ -111,9 +111,15 @@ $hasManifest = isset($manifest) && !empty($manifest);
 ?>
 <!DOCTYPE html>
 <html class="h-100" lang="en">
-	<head data-weekoverview="<?php echo printBoolean($weekOverview); ?>"
+	<head data-nextday="<?php echo $nextDay; ?>"
+				data-prevday="<?php echo $prevDay; ?>"
+				data-nextweek="<?php echo $nextWeek; ?>"
+				data-prevweek="<?php echo $prevWeek; ?>"
+				
+				data-weekoverview="<?php echo printBoolean($weekOverview); ?>"
 				data-highlightevents="<?php echo printBoolean($highlightEvents); ?>"
 				data-highlightclasses="<?php echo $highlightClasses; ?>"
+				
 				data-extraclasses="<?php echo $extraClasses; ?>"
 				data-enabletodaylink="<?php echo printBoolean($enableTodayLink);?>"
 				data-hasmanifest="<?php echo printBoolean($hasManifest); ?>"
