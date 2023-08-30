ef0650bf5c28da27763eedda43e53fefaef210aa
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2023-08-30 02:09:52 +0200

    Create ics.php

---
+++ ics.php
@@ -0,0 +1,34 @@
+<?php
+require_once("config.php");
+if(isset($icaltoken) && (!isset($_GET['icaltoken']) || $_GET['icaltoken'] != $icaltoken)) {
+    echo "No permission";
+    die;
+}
+
+require_once("include/globals.php");
+require_once("include/data-acquisition.php");
+
+$fullCalendar = true;
+require_once("include/data-processing.php");
+
+/* Define and output ical */
+$ical = "BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Schedule//EN";
+
+foreach($schedule as $event) {
+    $ical .= "
+BEGIN:VEVENT
+UID:" . md5(uniqid(mt_rand(), true)) . "
+DTSTAMP:" . gmdate("Ymd") . "T" . gmdate("His") . "Z
+DTSTART:" . date("Ymd\THis", strtotime($event['startDateTime'])) .  "
+DTEND:" . date("Ymd\THis", strtotime($event['endDateTime'])) . "
+SUMMARY:" . $event['subject'] . ", " . $event['room'] . "
+END:VEVENT";
+}
+
+header('Content-Type: text/calendar; charset=utf-8');
+header('Content-Disposition: attachment; filename=schedule.ics');
+
+echo $ical . "
+END:VCALENDAR";
